55f5ffac0b5f48d5d1c86e6b847e86c4e1612c87
Everita Vecberza <everita.vecberza@gmail.com>
2022-02-28 19:25:42 +0200

    Initial commit

---
+++ src/main/java/Animal/webapp/AnimalWebappApplication.java
@@ -0,0 +1,13 @@
+package Animal.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimalWebappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Animal/webapp/AnimalWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package Animal.webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
